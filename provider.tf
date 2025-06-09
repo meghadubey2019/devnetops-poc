666d29841ca3aac7d15b229d05a0d4d8d352a417
meghadubey2019 <156999004+meghadubey2019@users.noreply.github.com>
2025-06-09 06:50:38 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,4 @@
 provider "azurerm" {
   features {}
-
-subscription_id = ${{ secrets.ARM_SUBSCRIPTION_ID }}
-client_id       = ${{ secrets.ARM_CLIENT_ID }}
-client_secret   = ${{ secrets.ARM_CLIENT_SECRET }}
-tenant_id       = ${{ secrets.ARM_TENANT_ID }}
 }
 
